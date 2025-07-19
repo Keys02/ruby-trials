bdeb542b4e7c74eff0b40cb7ef34d16e29698e11
Keys <keys14052002@gmail.com>
2025-07-19 08:22:15 +0000

    Use join to combine interests

--- ruby-exercises/user_input.rb
+++ ruby-exercises/user_input.rb
@@ -4,28 +4,22 @@ interests = [];
 
   puts "Enter your interest"
   print "input> "
-  user_interest = gets.chomp.downcase;
-  
+  user_interest = gets.chomp;
+
   if user_interest != "q"
     interests.push(user_interest)
   end 
 
-while user_interest != "q"
+while user_interest.downcase != "q" or user_interest.downcase != "quit"
     puts "Enter your interest"
     print "input> "
-    user_interest = gets.chomp.downcase;
+    user_interest = gets.chomp
 
-    if user_interest != "q"
+    if user_interest.downcase != "q" or user_interest.downcase != "quit"
       interests.push(user_interest)
     end
 end
 
-interest_builder = "["
-
-interests.each do |interest|
-    interest_builder += "#{interest}, "
-end
-
-interest_builder += "]"
+interest_builder = interest.join(", ")
 
 puts interest_builder;
