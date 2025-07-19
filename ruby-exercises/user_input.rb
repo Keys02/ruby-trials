d56ffeb2f0102f8edfdf7af7ca1c8425116c8e06
Keys <keys14052002@gmail.com>
2025-07-19 08:08:53 +0000

    We made progress

--- ruby-exercises/user_input.rb
+++ ruby-exercises/user_input.rb
@@ -1,7 +1,31 @@
-interest = [];
-user_interest = gets.chmod.downcase;
+# A program which prompts the user to enter their interest and then it adds it to a list
 
-while (user_interest != "q")
-    user_interest = gets.chmod.downcase
-    interest[] = user_interest
+interests = [];
+
+  puts "Enter your interest"
+  print "input> "
+  user_interest = gets.chomp.downcase;
+  
+  if user_interest != "q"
+    interests.push(user_interest)
+  end 
+
+while user_interest != "q"
+    puts "Enter your interest"
+    print "input> "
+    user_interest = gets.chomp.downcase;
+
+    if user_interest != "q"
+      interests.push(user_interest)
+    end
 end
+
+interest_builder = "["
+
+interests.each do |interest|
+    interest_builder += "#{interest}, "
+end
+
+interest_builder += "]"
+
+puts interest_builder;
