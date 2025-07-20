88e476cda3148426f21cd0f3e3c0d631ee9efdcc
Keys <keys14052002@gmail.com>
2025-07-20 11:47:32 +0000

    Add a while loop construct to keep the app running

--- ruby_exercises/simple_calculator/calculator/simple_calculator.rb
+++ ruby_exercises/simple_calculator/calculator/simple_calculator.rb
@@ -17,42 +17,45 @@ end
 
 # Main calculator controller method
 def calculator_controller()
-  puts "What operation would you like to perform"
-  puts "+) Addition"
-  puts "-) Subtraction"
-  puts "*) Multiply"
-  puts "/) Division"
-  puts "q) quit"
+  while true
+    puts "What operation would you like to perform"
+    puts "+) Addition"
+    puts "-) Subtraction"
+    puts "*) Multiply"
+    puts "/) Division"
+    puts "q) quit"
 
-  command_operations = ["+", "-", "*", "/", "q"]
+    command_operations = ["+", "-", "*", "/", "q"]
 
-  print "input> "
-  command_operation = gets.chomp
+    print "input> "
+    command_operation = gets.chomp
 
-  if command_operation == "q"
-    puts "Goodbye!!"
-    abort
-  end
+    if command_operation.downcase == "q"
+      puts "Goodbye!!"
+      break
+    end
 
-  if !command_operations?includes(command_operation)
-    puts "I don't understand that command operation"
-  end
+    if !command_operations.includes?(command_operation)
+      puts "I don't understand that command operation"
+      next
+    end
 
-  puts "Enter the first number"
-  print "input> "
-  first_num = gets.chomp.to_f
+    puts "Enter the first number"
+    print "input> "
+    first_num = gets.chomp.to_f
 
-  puts "Enter the second number"
-  print "input> "
-  second_num = gets.chomp.to_f
+    puts "Enter the second number"
+    print "input> "
+    second_num = gets.chomp.to_f
 
-  if (operation == "+")
-    puts " Result: #{add(first_num, second_num)}"
-  elsif (operation == "-")
-    puts " Result: #{subtract(first_num, second_num)}"
-  elsif (operation == "*")
-    puts " Result: #{multiply(first_num, second_num)}"
-  else 
-    puts " Result: #{divide(first_num, second_num)}"
+    if (operation == "+")
+      puts " Result: #{add(first_num, second_num)}"
+    elsif (operation == "-")
+      puts " Result: #{subtract(first_num, second_num)}"
+    elsif (operation == "*")
+      puts " Result: #{multiply(first_num, second_num)}"
+    else 
+      puts " Result: #{divide(first_num, second_num)}"
+    end
   end
 end
