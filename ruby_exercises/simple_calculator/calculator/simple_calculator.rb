06e354de90b82101ca596f7476ca31867e7fae9e
Keys <keys14052002@gmail.com>
2025-07-20 11:43:02 +0000

    Add quit command

--- ruby_exercises/simple_calculator/calculator/simple_calculator.rb
+++ ruby_exercises/simple_calculator/calculator/simple_calculator.rb
@@ -17,6 +17,27 @@ end
 
 # Main calculator controller method
 def calculator_controller()
+  puts "What operation would you like to perform"
+  puts "+) Addition"
+  puts "-) Subtraction"
+  puts "*) Multiply"
+  puts "/) Division"
+  puts "q) quit"
+
+  command_operations = ["+", "-", "*", "/", "q"]
+
+  print "input> "
+  command_operation = gets.chomp
+
+  if command_operation == "q"
+    puts "Goodbye!!"
+    abort
+  end
+
+  if !command_operations?includes(command_operation)
+    puts "I don't understand that command operation"
+  end
+
   puts "Enter the first number"
   print "input> "
   first_num = gets.chomp.to_f
@@ -24,15 +45,6 @@ def calculator_controller()
   puts "Enter the second number"
   print "input> "
   second_num = gets.chomp.to_f
-
-  puts "What operation would you like to perform"
-  puts "+) Addition"
-  puts "-) Subtraction"
-  puts "*) Multiply"
-  puts "/) Division"
-
-  print "input> "
-  operation = gets.chomp
 
   if (operation == "+")
     puts " Result: #{add(first_num, second_num)}"
