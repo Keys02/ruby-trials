514abb117a0f331700b6145d9ebf2e809583c697
Keys <keys14052002@gmail.com>
2025-07-20 09:04:47 +0000

    Finished the temperature converter exercise

--- ruby-exercises/temperature-converter/temperature-converter.rb
+++ ruby-exercises/temperature-converter/temperature-converter.rb
@@ -1,17 +1,19 @@
 # A program to convert the temperature from Degree Celcius to Fahrenheit
 
-puts "Is the temperature unit in Degree Celcius of Fahrenheit"
+puts "Is the temperature unit in Degree Celcius or Fahrenheit"
 print "input> "
 unit = gets.chomp
 
-if unit.downcase != "f" || unit.downcase != "d" 
-    puts "You entered the wrong unit, please try again"
+if unit.downcase != "f" && unit.downcase != "d" 
+    puts "You entered the wrong unit"
 elsif unit.downcase == "f"
-    puts "Enter temperature"
-    temperature = gets.chomp
-    puts "Temperature in Fahrenheit: " + (temperature - 32) * 5/9
+    puts "Enter temperature (Fahrenheit)"
+    print "input> "
+    temperature = gets.chomp.to_f
+    puts "Temperature in Degree Celcius: " + (((temperature - 32) * 5/9)).round(4).to_s
 else
-    puts "Enter temperature"
-    temperature = gets.chomp
-    puts "Temperature in Degree Celcius: " + (temperature * 9/5) +32
+    puts "Enter temperature (Degree Celcius)"
+    print "input> "
+    temperature = gets.chomp.to_f.round(4)
+    puts "Temperature in Fahrenheit: " + (((temperature * 9/5) +32)).round(4).to_s
 end
