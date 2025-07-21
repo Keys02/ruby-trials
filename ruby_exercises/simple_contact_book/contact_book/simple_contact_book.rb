7a8c27f8d5f89e086bf20c0fb02c7560ad5c8633
Keys <keys14052002@gmail.com>
2025-07-21 15:50:41 +0000

    Define and implement functions that will process all user inputs

--- ruby_exercises/simple_contact_book/contact_book/simple_contact_book.rb
+++ ruby_exercises/simple_contact_book/contact_book/simple_contact_book.rb
@@ -16,8 +16,31 @@ def add_to_contacts(phone)
     contacts.write("\nName: #{name}, Phone: #{phone}")
 end
 
+def name_taker()
+    puts "Enter your name"
+    puts "input>"
+    name = gets.chomp
+    name
+end
+
+def phone_number_taker()
+    puts "Enter your phone number"
+    puts "input>"
+    phone_number = gets.chomp
+    phone_number
+end
+
+def welcome_screen()
+    puts "Welcome to the contact_book"  
+    puts "What do you want to do"
+    puts "1) Display all contacts"
+    puts "2) Add new contact"
+end
+
 def run()
-      
+    name = name_taker()
+    phone_number = phone_number_taker()
+    add_new_contact(name, phone_number)
 end
 
 get_all_contacts()
