8b2fe5137d1439c9550dbe93e1d67dd8ecd4e941
Keys <keys14052002@gmail.com>
2025-07-27 20:19:59 +0000

    Do a greet me exercise

--- learn.rb
+++ learn.rb
@@ -1,13 +1,4 @@
-puts "Floats arithmetic"
-puts 1.0 + 2.0
-puts 2.0 * 3.0
-puts 5.0 - 8.0
-puts 9.0 / 2.0
-
-print "\n"
-puts "Integers arithmetic"
-puts 1 + 2
-puts 2 * 3
-puts 5 - 8
-puts 9 / 2
-puts 5 / 2
+puts "Hello there, and what's your name?"
+name = gets.chomp
+puts "Your name is #{name}? What lovely name!";
+puts "Pleased to meet you, #{name}. :}"
---
+++ ruby_exercises/learn_to_program_book/greet_user/greet.rb
@@ -0,0 +1,8 @@
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your middle name"
+middle_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+
+puts "Welcome, #{first_name} #{middle_name} #{last_name}!!!"
