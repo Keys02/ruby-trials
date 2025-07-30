5705f7f90518d5548bbcaf468acb3953b6f09180
Keys <keys14052002@gmail.com>
2025-07-30 06:03:43 +0000

    Add fizz buzz exercise

---
+++ ruby_exercises/fizz_buzz/fizz_buzz.rb
@@ -0,0 +1,9 @@
+(1..100).each_with_index do |i|
+    if i % 2 == 0 && i % 5 ==0
+      puts "#{i} - FizzBuzz"
+    elsif(i % 2 == 0)
+      puts "#{i} - Fizz"
+    else
+      "#{i} - Buzz"
+    end
+end
