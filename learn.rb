a2194b6b966b8986c79952d31e950dc9997e3266
Keys <keys14052002@gmail.com>
2025-09-15 11:32:55 +0000

    project(angry_boss): add angry boss yelling project

--- learn.rb
+++ learn.rb
@@ -1,4 +1,1 @@
-first_name = "Opoku"
-last_name = "Chris"
-puts first_name + " " + last_name
-puts (-10).abs
+self.puts "Hello World"
---
+++ ruby_exercises/learn_to_program_book/angry_boss/angry_boss.rb
@@ -0,0 +1,4 @@
+puts "What do you want"
+print "> "
+want = gets.chomp
+puts "WHADDAYA MEAN \"I WANT A #{want.upcase}\"?!? YOU'RE FIRED"
