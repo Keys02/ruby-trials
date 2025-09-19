cf0e8d0fe0d4a1ff57cc5ace61d1087ad07a9ea4
Keys <keys14052002@gmail.com>
2025-09-19 03:08:26 +0000

    project(happy-birthday): C:\Ruby34-x64\bin\ruby.exe

--- ruby_exercises/learn_to_program_book/happy_birthday/happy_birthday.rb
+++ ruby_exercises/learn_to_program_book/happy_birthday/happy_birthday.rb
@@ -15,11 +15,8 @@ age = 1
 while Time.local(year + age, month, day) <= (Time.new)
   puts "🙂 - #{age}"
   # if the 2002/05/14 was provided as input for the year, month and day the while loop will run 23 times and on the last iteration the increment will still occur so we have to account for that.
-  if Time.local(year + age, month, day).year === (Time.new).year
-   break
-  end
-
-  age = age + 1
+  # break if Time.local(year + age, month, day).year === (Time.new).year
+  age = age + 1 unless Time.local(year + age, month, day).year === (Time.new).year
 end
 
 puts "You are #{age} years old" 
