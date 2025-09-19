2fc0de2bbf0dceddd0f4bdb582e93f165c971926
Keys <keys14052002@gmail.com>
2025-09-19 03:10:28 +0000

    refactor(happy-birthday): use best practices

--- ruby_exercises/learn_to_program_book/happy_birthday/happy_birthday.rb
+++ ruby_exercises/learn_to_program_book/happy_birthday/happy_birthday.rb
@@ -13,10 +13,11 @@ day = gets.chomp.to_i
 age = 1
 
 while Time.local(year + age, month, day) <= (Time.new)
-  puts "🙂 - #{age}"
+  puts "🙂"
   # if the 2002/05/14 was provided as input for the year, month and day the while loop will run 23 times and on the last iteration the increment will still occur so we have to account for that.
-  # break if Time.local(year + age, month, day).year === (Time.new).year
-  age = age + 1 unless Time.local(year + age, month, day).year === (Time.new).year
+  break if Time.local(year + age, month, day).year === (Time.new).year
+
+  age = age + 1
 end
 
 puts "You are #{age} years old" 
