2279c358edbd4a2812b56bd8ff9546ee3e28c662
Keys <keys14052002@gmail.com>
2025-10-03 13:42:52 +0000

    project-refactor(fibonacci): use parallel assignment to achieve the right sequence

--- ruby_exercises/pickaxe_ruby_book/fibonacci_sequence/fibonacci.rb
+++ ruby_exercises/pickaxe_ruby_book/fibonacci_sequence/fibonacci.rb
@@ -1,13 +1,11 @@
 def fibonacci_up_to(max)
-  # parallel assignment
-  i1, i2 = 1, 1
+  v1, v2 = 1,1
 
-  while i1 <= max
-    # yield i1
-    print i1, " "
-    i1, i2 = i2, i1 + i2
+  while(v1 <= max)
+    print v1, " "
+    # This Parallel assignment is key
+    v1, v2 = v2, v1 + v2
   end
 end
 
-# fibonacci_up_to(95) {}
-fibonacci_up_to(95)
+fibonacci_up_to(5)
