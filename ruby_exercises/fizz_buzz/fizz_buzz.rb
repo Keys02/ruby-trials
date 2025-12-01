7a6a1f2637eeabf30540affc76dfb76112c91085
Keys <keys14052002@gmail.com>
2025-12-01 02:11:23 +0000

    refactor(fizz_buzz): remove redundant parenthesis on a particular branching

--- ruby_exercises/fizz_buzz/fizz_buzz.rb
+++ ruby_exercises/fizz_buzz/fizz_buzz.rb
@@ -1,7 +1,7 @@
 (1..100).each_with_index do |i|
     if i % 2 == 0 && i % 5 == 0
       puts "#{i} - FizzBuzz"
-    elsif(i % 2 == 0)
+    elsif i % 2 == 0
       puts "#{i} - Fizz"
     else
       puts "#{i} - Buzz"
