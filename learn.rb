9781417b5c9db5902e5a15a2704ed83c51942665
Keys <keys14052002@gmail.com>
2026-01-07 02:16:58 +0000

    explore: define instance method for the class

--- learn.rb
+++ learn.rb
@@ -1,10 +1,18 @@
 class Song
-    attr_reader :state
-    attr_writer :name
+  attr_reader :state
+  attr_writer :title
     
-    def initialize(name)
-        @name = name
-    end
+  def initialize(title)
+    @title = title
+  end
+  
+  def play
+    put "Playing #{@title}"
+  end
+end
+
+def make
+    puts "What the hell are you doing here"
 end
 
 song1 = Song.new('Ruby Tuesday')
@@ -14,3 +22,4 @@ song3 = song1
 # An object and it's clone has the same object id
 puts song1.object_id
 puts song3.object_id
+song3.play
