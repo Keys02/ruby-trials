7b606766aed91a664f0741d375842a83854df324
Keys <keys14052002@gmail.com>
2026-01-15 10:21:57 +0000

    explore: master how to write blocks and how to pass them to other methods

--- learn.rb
+++ learn.rb
@@ -1,25 +1,2 @@
-class Song
-  attr_reader :state
-  attr_writer :title
-    
-  def initialize(title)
-    @title = title
-  end
-  
-  def play
-    put "Playing #{@title}"
-  end
-end
-
-def make
-    puts "What the hell are you doing here"
-end
-
-song1 = Song.new('Ruby Tuesday')
-song2 = Song.new('Evnveloped in Python')
-song3 = song1
-
-# An object and it's clone has the same object id
-puts song1.object_id
-puts song3.object_id
-song3.play
+animals = ['ant', 'bee', 'cat', 'dog']
+animals.each { |animal| print animal } # passing the block { |animal| print animal } to the method each
